a2c1c45a9ceb52aeef0f66b9373df2fee6552a7b
Anna Ludewig <al@codestryke.com>
2025-01-17 06:03:28 +0100

    login form validation

---
+++ client/src/components/form-error/FormError.tsx
@@ -0,0 +1,7 @@
+import { styled } from '@mui/material';
+
+export const FormError = styled('p')({
+  margin: 0,
+  fontSize: '12px',
+  color: 'red',
+});
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,14 +1,17 @@
 import { styled, TextField, Typography, useTheme } from '@mui/material';
 import axios from 'axios';
 import { useSnackbar } from 'notistack';
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import validator from 'validator';
+import { z } from 'zod';
 import { Button } from '../../components/button/Button';
+import { FormError } from '../../components/form-error/FormError';
+import { Link } from '../../components/link/Link';
 import { PageContainer } from '../../components/page-container/PageContainer';
 import { PageHeader } from '../../components/page-header/PageHeader';
 import { PaperCard } from '../../components/paper-card/PaperCard';
 import { routes } from '../../config/navigation/navigation';
 import { handleAxiosError } from '../../utils/error-handling/errorHandling';
-import { Link } from '../../components/link/Link';
 
 const StyledForm = styled('form')({
   width: '100%',
@@ -23,14 +26,35 @@ const initialUserData = {
   password: '',
 };
 
+const emailParser = z
+  .string()
+  .refine((value) => validator.isEmail(value), { message: 'A valid email is required' });
+
 export const Login = () => {
   const theme = useTheme();
   const { enqueueSnackbar } = useSnackbar();
   const [userData, setUserData] = useState(initialUserData);
+  const [error, setError] = useState(initialUserData);
+
+  const validatedEmail = useMemo(() => emailParser.safeParse(userData.email), [userData.email]);
+
+  useEffect(() => {
+    if (validatedEmail.data) {
+      setError({ ...error, email: '' });
+    }
+  }, [validatedEmail, error]);
+
+  useEffect(() => {
+    if (userData.password.length > 0) {
+      setError({ ...error, password: '' });
+    }
+  }, [userData.password, error]);
+
+  const isSubmitDisabled = !validatedEmail.data || !userData.password;
 
   const handleLogin = async () => {
     try {
-      const result = await axios.post(`${import.meta.env.VITE_AUTH_URL}${routes.login.route}`, userData, {
+      await axios.post(`${import.meta.env.VITE_AUTH_URL}${routes.login.route}`, userData, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -41,26 +65,52 @@ export const Login = () => {
     }
   };
 
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: 'email' | 'password'
+  ) => {
+    const value = event.target.value;
+    setUserData({ ...userData, [field]: value });
+  };
+
   return (
     <PageContainer>
       <PaperCard maxWidth="500px" padding="50px">
         <PageHeader title="Login" textAlign="center" />
         <StyledForm>
-          <TextField
-            name="email"
-            label="E-Mail"
-            type="email"
-            value={userData.email}
-            onChange={(event) => setUserData({ ...userData, email: event.target.value })}
-          />
-          <TextField
-            name="password"
-            label="Enter Password"
-            type="password"
-            value={userData.password}
-            onChange={(event) => setUserData({ ...userData, password: event.target.value })}
-          />
-          <Button onClick={handleLogin}>Login</Button>
+          <div>
+            <TextField
+              name="email"
+              label="E-Mail"
+              type="email"
+              value={userData.email}
+              required
+              fullWidth
+              onBlur={() =>
+                setError({ ...error, email: validatedEmail.data ? '' : 'A valid Email is required' })
+              }
+              onChange={(event) => handleChange(event, 'email')}
+              error={!!error.email}
+            />
+            {error.email && <FormError>{error.email}</FormError>}
+          </div>
+          <div>
+            <TextField
+              name="password"
+              label="Enter Password"
+              type="password"
+              value={userData.password}
+              required
+              fullWidth
+              onBlur={() => setError({ ...error, password: userData.password ? '' : 'Password is required' })}
+              onChange={(event) => handleChange(event, 'password')}
+              error={!!error.password}
+            />
+            {error.password && <FormError>{error.password}</FormError>}
+          </div>
+          <Button onClick={handleLogin} disabled={isSubmitDisabled}>
+            Login
+          </Button>
           <Typography variant="body2">
             Not registered yet?{' '}
             <Link
