a83996c921949fd8d230e15ca5ba5a57fb5497c4
Anna Ludewig <al@codestryke.com>
2025-01-17 01:26:08 +0100

    hash password

--- auth/controllers/controllers.ts
+++ auth/controllers/controllers.ts
@@ -5,33 +5,42 @@ import fs from 'fs';
 import jwt, { SignOptions } from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import { loginInputParser, registerInputParser } from '../helpers/parameterParser';
+import bcrypt from 'bcrypt';
+import { verify } from 'crypto';
 
 type UnwrapModel<T> = T extends Model<infer U> ? U : never;
 type IUser = UnwrapModel<typeof User> & { _id: ObjectId };
 
 type GetUserIdInput = {
-  email?: string;
-  userName?: string;
-  password?: string;
+  email: string;
+  password: string;
 };
 
-const getUserId = async ({ email, userName, password }: GetUserIdInput) => {
-  console.log('Retrieving user from DB')
-  let user: IUser[] | null = null;
-  user = await User.find({ email: email, password: password });
-  if (user.length === 0) {
+const getUserId = async ({ email, password }: GetUserIdInput) => {
+  console.log('Retrieving user from DB');
+  let user: IUser | null = null;
+  const result = await User.find({ email: email });
+  if (result.length === 0) {
     throw new MongooseError('Incorrect login data');
-  } else if (user.length > 1) {
+  } else if (result.length > 1) {
     throw new MongooseError('More than one user found for the provided login data');
-  } else {
-    console.log('Found user in DB:')
-    console.log(user)
-    return user[0]._id;
+  }
+  user = result[0];
+  console.log('Found user in DB');
+  try {
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches) {
+      throw new MongooseError('Incorrect login data');
+    }
+    return user._id;
+  } catch (error) {
+    console.error(error);
+    throw new MongooseError('Incorrect login data');
   }
 };
 
 const generateJwt = (userId: ObjectId) => {
-  console.log('generating token')
+  console.log('generating token');
   const signOptions: SignOptions = {
     expiresIn: '30s',
     algorithm: 'RS256',
@@ -62,10 +71,17 @@ export const login = async (req: Request, res: Response) => {
   }
 };
 
+const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+
 export const register = async (req: Request, res: Response) => {
   try {
     const signUpInformation = registerInputParser.parse(req.body);
-    const newUser = await User.create(signUpInformation);
+    const hashedPassword = await hashPassword(signUpInformation.password);
+    const newUser = await User.create({ ...signUpInformation, password: hashedPassword });
     if (!newUser) {
       res.status(500).json({ message: 'Could not create new user' });
     }
